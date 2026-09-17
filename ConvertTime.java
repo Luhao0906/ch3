392db3754ab86912f83a88354d6a4f3a24441018
luhao <2777096@learn.vsb.bc.ca>
2026-09-17 12:35:05 -0700

    Add files via upload

--- ConvertTime.java
+++ ConvertTime.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 
-public class Temperature{
+public class ConvertTime{
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
-		System.out.print("enter how many seconds:")
-		int 
+		System.out.print("enter how many seconds:");
+		int seconds = in.nextInt();
+		int hours = seconds/3600;
+		int minutes = seconds%3600/60;
+		int seconds2 = seconds-(hours*3600)-(minutes*60);
+		System.out.printf("%d hours, %d minutes, %d seconds", hours, minutes, seconds2);
+		
+		
 		
 		}
 	}
